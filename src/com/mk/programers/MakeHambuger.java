104820cb1b73f02734264462504a61a21d9e0892
myungkookchoi <audrnr3577@gmail.com>
2023-07-20 01:23:01 +0900

    make the hambuger
    Signed-off-by: myungkookchoi <audrnr3577@gmail.com>

---
+++ src/com/mk/programers/MakeHambuger.java
@@ -0,0 +1,21 @@
+package com.mk.programers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MakeHambuger {
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+//        int[] ingredient = {1, 3, 2, 1, 2, 1, 3, 1, 2};
+        System.out.println(solution(ingredient));
+    }
+
+    public static int solution(int[] ingredient) {
+        int answer = ingredient.length;
+
+
+
+        return answer;
+    }
+}
